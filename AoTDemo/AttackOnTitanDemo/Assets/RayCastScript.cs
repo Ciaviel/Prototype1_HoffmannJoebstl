d7eeddab12835999fb9baa1b23eba41525ef3b8d
Johannes Jöbstl <jjoebstl@gmx.net>
2015-10-22 16:23:33 +0200

    JJ - fixes and improvements Rotation of Direction Vectors still buggy

--- AoTDemo/AttackOnTitanDemo/Assets/RayCastScript.cs
+++ AoTDemo/AttackOnTitanDemo/Assets/RayCastScript.cs
@@ -14,11 +14,19 @@ public class RayCastScript : MonoBehaviour {
     Vector3 targetLeft;
     Vector3 targetRight;
 
+    Vector3 DirectionLeft;
+    Vector3 DirectionRight;
+
 	// Use this for initialization
 
     void Update()
     {
-
+        if (Input.GetButton("Fire1")) Debug.LogError("Fire1");
+        if (Input.GetButton("Fire2")) Debug.LogError("Fire2");
+        if (Input.GetButton("Fire3")) Debug.LogError("Fire3");
+        if (Input.GetButton("Jump")) Debug.LogError("Jump");
+        if (Input.GetButton("LB")) Debug.LogError("LB");
+        if (Input.GetButton("RB")) Debug.LogError("RB");
     }
 	
 	void FixedUpdate () {
@@ -65,62 +73,75 @@ public class RayCastScript : MonoBehaviour {
             }
         }
 
-        if (Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonUp(0) || Input.GetButtonUp("LB"))
         {
             if (targetLeft == Vector3.zero)
             {
                 targetLeft = hitLeft.point;
+                Vector3 Direction = Vector3.Cross(targetLeft - transform.position, Vector3.up).normalized;
+                if (Mathf.Sign(Vector3.Dot(transform.forward, Direction)) == -1)
+                {
+                    Direction = Quaternion.Euler(0.0f, 180.0f, 0.0f) * Direction;
+                }
+                transform.parent.GetComponent<Rigidbody>().velocity = Direction * transform.parent.GetComponent<Rigidbody>().velocity.magnitude;
             }
-            //else
-            //{
-            //    targetLeft = Vector3.zero;
-            //}
-            Debug.LogError(targetLeft);
+            else
+            {
+                targetLeft = Vector3.zero;
+                DirectionLeft = Vector3.zero;
+            }
             
         }
-        if (Input.GetMouseButtonUp(1))
+        if (Input.GetMouseButtonUp(1) || Input.GetButtonUp("RB"))
         {
             if (targetRight == Vector3.zero)
             {
                 targetRight = hitRight.point;
+                Vector3 Direction = Vector3.Cross(targetRight - transform.position, Vector3.up).normalized;
+                if (Mathf.Sign(Vector3.Dot(transform.forward, Direction)) == -1)
+                {
+                    Direction = Quaternion.Euler(0.0f, 180.0f, 0.0f) * Direction;
+                }
+                transform.parent.GetComponent<Rigidbody>().velocity = Direction * transform.parent.GetComponent<Rigidbody>().velocity.magnitude;
             }
-            //else
-            //{
-            //    targetRight = Vector3.zero;
-            //}
-            Debug.LogError(targetRight);
+            else
+            {
+                targetRight = Vector3.zero;
+                DirectionRight = Vector3.zero;
+            }
         }
 
         if (targetLeft != Vector3.zero)
         {
-            Vector3 Direction = Vector3.Cross(transform.position - hitLeft.point, Vector3.up).normalized;
-            if (Mathf.Sign(Vector3.Dot(transform.forward, Vector3.Cross(transform.position - hitLeft.point, Vector3.up).normalized)) == -1)
+            DirectionLeft = Vector3.Cross(targetLeft - transform.position, Vector3.up).normalized;
+            if (Mathf.Sign(Vector3.Dot(transform.parent.GetComponent<Rigidbody>().velocity - transform.position, DirectionLeft)) == -1)
             {
-                Direction = Quaternion.Euler(0.0f, 180.0f, 0.0f) * Direction;
+                DirectionLeft = Quaternion.Euler(0.0f, 180.0f, 0.0f) * DirectionLeft;
             }
-            //transform.parent.position += Direction * 0.2f;
-            transform.parent.GetComponent<Rigidbody>().AddForce(Direction * 10.0f, ForceMode.VelocityChange);
 
-            Debug.DrawRay(transform.position, hitLeft.point - transform.position, Color.red);
-            Debug.DrawLine(hitLeft.point, hitLeft.point + Vector3.up, Color.blue);
-            Debug.DrawLine(hitLeft.point, hitLeft.point + Direction, Color.green);
+            Debug.DrawRay(transform.position, targetLeft - transform.position, Color.red);
+            Debug.DrawLine(targetLeft, targetLeft + transform.up, Color.blue);
+            Debug.DrawLine(targetLeft, targetLeft + DirectionLeft, Color.green);
         }
 
         if (targetRight != Vector3.zero)
         {
-            Vector3 Direction = Vector3.Cross(transform.position - hitRight.point, Vector3.up).normalized;
-            if (Mathf.Sign(Vector3.Dot(transform.forward, Vector3.Cross(transform.position - hitRight.point, Vector3.up).normalized)) == -1)
+            DirectionRight = Vector3.Cross(targetRight - transform.position, Vector3.up).normalized;
+            if (Mathf.Sign(Vector3.Dot(transform.parent.GetComponent<Rigidbody>().velocity - transform.position, DirectionRight)) == -1)
             {
-                Direction = Quaternion.Euler(0.0f, 180.0f, 0.0f) * Direction;
+                DirectionRight = Quaternion.Euler(0.0f, 180.0f, 0.0f) * DirectionRight;
             }
-            //transform.parent.position += Direction * 0.2f;
-            transform.parent.GetComponent<Rigidbody>().AddForce(Direction * 10.0f, ForceMode.VelocityChange);
+            
 
-            Debug.DrawRay(transform.position, hitRight.point - transform.position, Color.red);
-            Debug.DrawLine(hitRight.point, hitRight.point + Vector3.up, Color.blue);
-            Debug.DrawLine(hitRight.point, hitRight.point + Direction, Color.green);
+            Debug.DrawRay(transform.position, targetRight - transform.position, Color.red);
+            Debug.DrawLine(targetRight, targetRight + transform.up, Color.blue);
+            Debug.DrawLine(targetRight, targetRight + DirectionRight, Color.green);
         }
 
-        //Debug.LogError("Left: " + Vector3.Dot(transform.forward, Vector3.Cross(transform.position - hitLeft.point, Vector3.up).normalized) + " Right: " + Vector3.Dot(transform.forward, Vector3.Cross(transform.position - hitRight.point, Vector3.up).normalized));
+        Vector3 Dir = DirectionLeft + DirectionRight;
+        if(Dir != Vector3.zero) 
+            transform.parent.GetComponent<Rigidbody>().velocity = Dir.normalized * transform.parent.GetComponent<Rigidbody>().velocity.magnitude;
+
+        Debug.DrawLine(transform.position, transform.position + transform.parent.GetComponent<Rigidbody>().velocity, Color.white);
 	}
 }
