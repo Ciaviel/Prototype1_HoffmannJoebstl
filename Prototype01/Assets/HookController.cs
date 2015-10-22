ba8e4c1c752042981ace949cbdf22ed8838528ce
Tobias Hoffmann <thoffmann.mmt-m2015@fh-salzburg.ac.at>
2015-10-22 14:25:00 +0200

    moins;

---
+++ Prototype01/Assets/HookController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HookController : MonoBehaviour
+{
+
+    private Rigidbody myRigidbody;
+
+    public Transform target;
+
+    public float camspeed;
+
+    private Quaternion targetLookDir;
+
+    // Use this for initialization
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //pick target
+
+
+
+        //if (Input.GetButtonDown("Fire1"))
+        {
+            if (myRigidbody.velocity.sqrMagnitude > 0.1f)
+            {
+                targetLookDir = Quaternion.LookRotation(myRigidbody.velocity, Vector3.up);
+            }
+            else
+            {
+                targetLookDir = Quaternion.LookRotation(transform.forward, Vector3.up);
+
+            }
+        }
+
+        if (Input.GetButton("Fire1"))
+        {
+            Vector3 dir = target.position - transform.position;
+            dir.Normalize();
+
+            Vector3 radVel = Vector3.Project(myRigidbody.velocity, dir);
+
+            //Vector3 remain = myRigidbody.velocity - radVel;
+
+
+            myRigidbody.AddForce(-radVel, ForceMode.VelocityChange);
+            //myRigidbody.AddForce(remain.normalized*radVel.magnitude, ForceMode.VelocityChange);
+
+
+
+            //float distToTarget = (target.position - transform.position).magnitude;
+
+            //Vector3 dir = target.position - transform.position;
+            //dir.Normalize();
+
+            //myRigidbody.AddForce(dir * force, ForceMode.VelocityChange);
+        }
+
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetLookDir, camspeed);
+    }
+}
