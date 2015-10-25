77f33ef1151f874c16c91078a48deca898067e22
Tobias Hoffmann <thoffmann.mmt-m2015@fh-salzburg.ac.at>
2015-10-25 13:44:57 +0100

    AUSHFFKJLSAHLD

---
+++ Prototype01/Assets/CamController.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamController : MonoBehaviour
+{
+    public HookController hookController;
+    
+    public Vector3 ray1Offset;
+    public Vector3 ray1Dir;
+
+    public Vector3 ray2Offset;
+    public Vector3 ray2Dir;
+
+    public GameObject debugBall1;
+    public GameObject debugBall2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float camX = Input.GetAxis("HorizontalSec");
+        float camY = Input.GetAxis("VerticalSec");
+
+        transform.localRotation *= Quaternion.AngleAxis(camX * 2, Vector3.up);
+        transform.localRotation *= Quaternion.AngleAxis(camY * 2, Vector3.right);
+
+        if (!Input.GetButton("Fire1"))
+        {
+            Ray ray1 = new Ray(transform.position + transform.parent.rotation * ray1Offset, transform.rotation * ray1Dir );
+        RaycastHit result1;
+        Physics.Raycast(ray1, out result1);
+
+        if(result1.collider != null)
+        {
+            Debug.DrawLine(transform.position + transform.parent.rotation * ray1Offset, result1.point, Color.green);
+            debugBall1.transform.position = result1.point;
+            debugBall1.SetActive(true);
+            hookController.target1 = debugBall1.transform;
+        }
+        else
+        {
+            debugBall1.SetActive(false);
+            hookController.target1 = null;
+            }
+        }
+        else
+        {
+            Debug.DrawLine(transform.position + transform.parent.rotation * ray1Offset, debugBall1.transform.position, Color.blue);
+        }
+
+        if (!Input.GetButton("Fire2"))
+        {
+            Ray ray2 = new Ray(transform.position + transform.parent.rotation * ray2Offset, transform.rotation * ray2Dir);
+            RaycastHit result2;
+            Physics.Raycast(ray2, out result2);
+
+            if (result2.collider != null)
+            {
+                Debug.DrawLine(transform.position + transform.parent.rotation * ray2Offset, result2.point, Color.green);
+                debugBall2.transform.position = result2.point;
+                debugBall2.SetActive(true);
+                hookController.target2 = debugBall2.transform;
+            }
+            else
+            {
+                debugBall2.SetActive(false);
+                hookController.target2 = null;
+            }
+        }
+        else
+        {
+            Debug.DrawLine(transform.position + transform.parent.rotation * ray2Offset, debugBall2.transform.position, Color.blue);
+        }
+
+    }
+}
--- Prototype01/Assets/HookController.cs
+++ Prototype01/Assets/HookController.cs
@@ -6,62 +6,121 @@ public class HookController : MonoBehaviour
 
     private Rigidbody myRigidbody;
 
-    public Transform target;
+    public Vector3 myUp;
 
-    public float camspeed;
+    public float ropeForce;
+    public float jumpforce;
 
+    public Transform target1;
+    public Transform target2;
+
+    private float distToTarget1;
+    private float distToTarget2;
+
+    public float turnspeed;
     private Quaternion targetLookDir;
 
     // Use this for initialization
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody>();
+        myUp = Vector3.up;
+
+        targetLookDir = Quaternion.LookRotation(transform.forward, myUp);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        //pick target
-
-
-
-        //if (Input.GetButtonDown("Fire1"))
+        if (transform.up != myUp)
         {
-            if (myRigidbody.velocity.sqrMagnitude > 0.1f)
-            {
-                targetLookDir = Quaternion.LookRotation(myRigidbody.velocity, Vector3.up);
-            }
-            else
-            {
-                targetLookDir = Quaternion.LookRotation(transform.forward, Vector3.up);
-
-            }
+            //look straight
+            targetLookDir = Quaternion.LookRotation(transform.forward - Vector3.Project(transform.forward, myUp.normalized), myUp);
         }
 
-        if (Input.GetButton("Fire1"))
+        if (Input.GetButtonDown("Jump"))
         {
-            Vector3 dir = target.position - transform.position;
+            myRigidbody.AddForce(myUp * jumpforce, ForceMode.VelocityChange);
+        }
+
+        Debug.Log(Input.GetAxis("Trigger1") + "   " + Input.GetAxis("Trigger2"));
+
+
+        if (Input.GetButton("Fire1") && target1 != null)
+        {
+
+            if (myRigidbody.velocity.sqrMagnitude > 0.1f)
+            {
+                targetLookDir = Quaternion.LookRotation(myRigidbody.velocity, myUp);
+            }
+
+            Vector3 dir = target1.position - transform.position;
+
+
+            if (distToTarget1 <= 0.0f)
+            {
+                distToTarget1 = dir.magnitude;
+            }
+
+            if (dir.magnitude > distToTarget1)
+            {
+                transform.position = target1.position - dir.normalized * distToTarget1;
+            }
+
             dir.Normalize();
 
             Vector3 radVel = Vector3.Project(myRigidbody.velocity, dir);
-
-            //Vector3 remain = myRigidbody.velocity - radVel;
-
-
-            myRigidbody.AddForce(-radVel, ForceMode.VelocityChange);
-            //myRigidbody.AddForce(remain.normalized*radVel.magnitude, ForceMode.VelocityChange);
-
-
-
-            //float distToTarget = (target.position - transform.position).magnitude;
-
-            //Vector3 dir = target.position - transform.position;
-            //dir.Normalize();
-
-            //myRigidbody.AddForce(dir * force, ForceMode.VelocityChange);
+            if (radVel.magnitude > 0.0f && Vector3.Dot(radVel, dir) < 0.0f )
+            {
+                myRigidbody.AddForce(-radVel, ForceMode.VelocityChange);
+            }
+            else
+            {
+                myRigidbody.AddForce(dir * Input.GetAxis("Trigger1") * ropeForce, ForceMode.Acceleration);
+            }
+        }
+        else
+        {
+            distToTarget1 = -1.0f;
         }
 
+        if (Input.GetButton("Fire2") && target2 != null)
+        {
+            if (myRigidbody.velocity.sqrMagnitude > 0.1f)
+            {
+                targetLookDir = Quaternion.LookRotation(myRigidbody.velocity, myUp);
+            }
 
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetLookDir, camspeed);
+            Vector3 dir = target2.position - transform.position;
+
+            if (distToTarget2 <= 0.0f)
+            {
+                distToTarget2 = dir.magnitude;
+            }
+
+            if (dir.magnitude > distToTarget2)
+            {
+                transform.position = target2.position - dir.normalized * distToTarget2;
+            }
+
+            dir.Normalize();
+
+            Vector3 radVel = Vector3.Project(myRigidbody.velocity, dir);
+            if (Vector3.Dot(radVel, dir) < 0.0f)
+            {
+                myRigidbody.AddForce(-radVel, ForceMode.VelocityChange);
+            }
+            else
+            {
+                myRigidbody.AddForce(dir * Input.GetAxis("Trigger2") * ropeForce, ForceMode.Acceleration);
+            }
+        }
+        else
+        {
+            distToTarget2 = -1.0f;
+        }
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetLookDir, turnspeed);
     }
+
 }
